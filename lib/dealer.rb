6b52365660e19c3c315415233689a4129ff043b2
wthoma22 <w.thomas390@yahoo.com>
2017-03-22 20:03:42 -0600

    Third Commit

--- lib/dealer.rb
+++ lib/dealer.rb
@@ -1,14 +1,40 @@
+require 'pry'
 class Dealer
+  
+attr_reader :inventory, :all_makes
 
-  attr_reader :inventory, :all_makes, :all_year
-
-  def initialize(year, all_makes)
-    @all_makes = make
+  def initialize
     @inventory = []
-    @all_years = year
+    @all_makes = []
   end
+
 
   def add_inventory(new_car)
-    @inventory << new_car
+    inventory << new_car
   end
+
+  def all_makes
+    inventory.map do |car|
+      car.make
+      end
+    end
+
+  def all_years
+    inventory.map do |car|
+    car.year
+    end
+  end
+
+  def find_first(car)
+    inventory.find do |auto|
+    auto.make == car
+    end
+  end
+
+  def find_many(car)
+    inventory.find_all do |auto|
+    auto.make == car
+    end
+  end
+
 end
